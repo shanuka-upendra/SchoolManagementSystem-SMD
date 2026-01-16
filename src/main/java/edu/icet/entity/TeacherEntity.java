2e34b52e6dbf59ba792417b6a3c21df7623318a7
ShanukaUpendraOfficial <shanukaupendra@gmail.com>
2026-01-17 00:12:54 +0530

    feat : add Teacher Entity to manage relationships

--- src/main/java/edu/icet/entity/TeacherEntity.java
+++ src/main/java/edu/icet/entity/TeacherEntity.java
@@ -1,17 +1,21 @@
-package edu.icet.dto;
+package edu.icet.entity;
 
 import edu.icet.gender.Gender;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-public class TeacherDto{
+@Entity
+@Table(name = "teacher_table")
+public class TeacherEntity {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Integer teacherId;
     private String title;
     private String name;
