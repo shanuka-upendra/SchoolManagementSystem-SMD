b4bd98351137e73b7ab162122d0feee9c93fdf80
ShanukaUpendraOfficial <shanukaupendra@gmail.com>
2026-01-17 00:04:53 +0530

    add : teacher into controller and Dto

---
+++ src/main/java/edu/icet/controller/TeacherController.java
@@ -0,0 +1,34 @@
+package edu.icet.controller;
+
+import edu.icet.dto.TeacherDto;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class TeacherController {
+
+
+     public void addTeacher(TeacherDto teacherDto){
+
+     }
+
+     public void updateTeacher(TeacherDto teacherDto){
+
+     }
+
+     public void deleteTeacher(Integer id){
+
+     }
+
+     public TeacherDto searchTeacherById(Integer id){
+
+         return null;
+     }
+
+     public List<TeacherDto> searchAll(){
+
+         return null;
+     }
+}
+
---
+++ src/main/java/edu/icet/dto/TeacherDto.java
@@ -0,0 +1,8 @@
+package edu.icet.dto;
+
+public class TeacherDto {
+    private Integer teacherId;
+    private String title;
+    private String name;
+    private String subjectStream;
+}
