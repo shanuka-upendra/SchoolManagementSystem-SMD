b49d5ba36f01501b51184cd12c7332502cd13f64
ShanukaUpendraOfficial <shanukaupendra@gmail.com>
2026-02-04 03:06:02 +0530

    feat : implement Timetable feature.

--- src/main/java/edu/icet/service/impl/TimetableServiceImpl.java
+++ src/main/java/edu/icet/service/impl/TimetableServiceImpl.java
@@ -1,34 +1,56 @@
 package edu.icet.service.impl;
 
 import edu.icet.dto.TimetableDto;
+import edu.icet.entity.TimetableEntity;
+import edu.icet.repository.TimetableRepository;
 import edu.icet.service.TimetableService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@RequiredArgsConstructor
+@Service
 public class TimetableServiceImpl implements TimetableService {
+
+    final TimetableRepository repository;
+
+    final ModelMapper mapper;
 
     @Override
     public void addTimetable(TimetableDto timetableDto) {
-
+        repository.save(mapper.map(timetableDto, TimetableEntity.class));
     }
 
     @Override
     public void updateTimetable(TimetableDto timetableDto) {
-
+        repository.save(mapper.map(timetableDto, TimetableEntity.class));
     }
 
     @Override
     public void deleteTimetable(Integer id) {
+        repository.deleteById(id);
 
     }
 
     @Override
     public TimetableDto searchTimetableById(Integer id) {
-        return null;
+        TimetableEntity all = repository.findById(id).get();
+        TimetableDto timetableDto = mapper.map(all,TimetableDto.class);
+
+        return timetableDto;
     }
 
     @Override
     public List<TimetableDto> getAll() {
-        return List.of();
+        List<TimetableEntity> all = repository.findAll();
+        List<TimetableDto> timetableDto = new ArrayList<>();
+
+        all.forEach(timetableEntity -> {
+            timetableDto.add(mapper.map(timetableEntity,TimetableDto.class));
+        });
+        return timetableDto;
     }
 }
