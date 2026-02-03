841161badf9eca70f3db33db9434c3d78243c483
ShanukaUpendraOfficial <shanukaupendra@gmail.com>
2026-02-03 23:03:22 +0530

    feat : Implement add,update and delete class functions.

--- src/main/java/edu/icet/controller/ClassController.java
+++ src/main/java/edu/icet/controller/ClassController.java
@@ -3,10 +3,7 @@ package edu.icet.controller;
 import edu.icet.dto.ClassDto;
 import edu.icet.service.ClassService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,22 +12,22 @@ public class ClassController {
     final ClassService classService;
 
     @PostMapping("/add-class")
-    public void addClass(ClassDto classDto){
+    public void addClass(@RequestBody ClassDto classDto){
         classService.addClass(classDto);
     }
 
     @PostMapping("/update-class")
-    public void updateClass(ClassDto classDto){
+    public void updateClass(@RequestBody ClassDto classDto){
         classService.updateClass(classDto);
     }
 
-    @DeleteMapping("/delete-class")
-    public void deleteClass(Integer id){
+    @DeleteMapping("/delete-class/{id}")
+    public void deleteClass(@PathVariable Integer id){
         classService.deleteClass(id);
     }
 
     @GetMapping("/search-all")
-    public ClassDto searchClassById(Integer id){
+    public ClassDto searchClassById(@RequestBody Integer id){
         return classService.searchClassById(id);
     }
 }
